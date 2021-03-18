dc298b60862211884dabc906393cffb0424c9a73
shangoyanyi <shangoyanyi@gmail.com>
2021-03-18 17:14:00 +0800

    add HelloCtrl and HelloCtrlTest

---
+++ src/main/java/me/demo/springbootexample/HelloCtrl.java
@@ -0,0 +1,13 @@
+package me.demo.springbootexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+  @GetMapping
+  public String hello() {
+    return "Hello World i'm build in Azure Pipelines!";
+  }
+}
